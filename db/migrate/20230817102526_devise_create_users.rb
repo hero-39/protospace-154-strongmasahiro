512f0c590038ded08a8b86e150d14b9e66263e5c
kotomiyashita <kotomi.owo@gmail.com>
2023-08-17 21:26:41 +0900

    userモデルにバリデーションを追加

--- db/migrate/20230817102526_devise_create_users.rb
+++ db/migrate/20230817102526_devise_create_users.rb
@@ -4,8 +4,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :email,              null: false, unique: true, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :name,               null: false, default: ""
+      t.text :profile,              null: false
+      t.text :occupation,           null: false
+      t.text :position,             null: false
 
       ## Recoverable
       t.string   :reset_password_token
