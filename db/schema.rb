c9da7c4c3ccda0b411ca23d2aa0462ea310bf5eb
kotomiyashita <kotomi.owo@gmail.com>
2023-08-21 18:20:17 +0900

    コメント投稿機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_17_102526) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_21_070235) do
+  create_table "comments", charset: "utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "prototype_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
